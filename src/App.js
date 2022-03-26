ec48c2c520870b03d581ca7c90143a3d1dc55b17
aarontkrieg <56848457+aarontkrieg@users.noreply.github.com>
2022-03-25 17:36:55 -0700

    fixed errors in the code, register should function correctly when registering users

--- src/App.js
+++ src/App.js
@@ -25,14 +25,10 @@ const particlesOptions = {
       }
     }
   }
-
-
 }
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      input: '',
+
+const initialState = {
+        input: '',
       imageUrl:'',
       box: {},
       route: 'signin',
@@ -44,7 +40,12 @@ class App extends Component {
         entries: 0,
         joined: ''
       }
-    }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = initialState;
   }
 
   loadUser = (data) =>{
@@ -99,6 +100,7 @@ class App extends Component {
         .then(count => {
           this.setState(Object.assign(this.state.user, { entries: count}))
             })
+        .catch(console.log)
       }
       this.displayFaceBox(this.calculateFaceLocation(response))
     })
@@ -107,7 +109,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout')  {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if(route === 'home') {
       this.setState({isSignedIn: true})
     }
